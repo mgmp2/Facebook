85d927d4e661998ee9352339e18b16d39b8ed464
Miriam Mendoza <miriammp1997@gmail.com>
2017-03-09 18:37:30 -0500

    validation of users perfect!

--- js/main.js
+++ js/main.js
@@ -16,9 +16,11 @@
         this.href += "?nombre="+saludo.textContent;
       })
 */
+var correo      = document.getElementById("mail");
+var contra      = document.getElementById("pass");
+
+
 function validar(){
-  var correo      = document.getElementById("mail");
-  var contra      = document.getElementById("pass");
 
   var expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
   var expRegPass  = /^[a-zA-z]{6,}$/;
@@ -26,34 +28,54 @@ function validar(){
   var emailError  = document.getElementById("errorEmail");
   var passError   = document.getElementById("errorPass");
   //e-mail
-  if(!expRegEmail.test(correo.value)){
+
+  if(!expRegEmail.test(correo.value)) {
+    if(!correo.value){
+      emailError.innerHTML= "El campo de usuario no puede estar en blanco";
+      correo.focus();
+    } else {
+      emailError.innerHTML= "Ingrese correo válido";
+      correo.focus();
+    }
+  } else if(!expRegPass.test(contra.value)){
+    if(!contra.value){
+      passError.innerHTML = " El campo de contraseña no puede estar en blanco";
+      passError.focus();
+    } else {
+      passError.innerHTML = "Ingrese contraseña válida";
+      passError.focus();
+    }
+  } else {
     passError.innerHTML = "";
-    emailError.innerHTML= "Ingrese correo válido";
-    correo.focus();
-
-
-
-              }
-  else if(!expRegPass.test(contra.value)){
     emailError.innerHTML= "";
-    passError.innerHTML = "Ingrese contraseña válida";
-    passError.focus();
 
-
-
-  }
-  else{
-    passError.innerHTML = "";
-    emailError.innerHTML= "";
-    alert("");
-    validarCuentas(correo.value);
+    validarCuentas(correo.value, contra.value);
   }
 
 }
 
-function validarCuentas(){
-  var correos = {
-                [nombre:"" ]
-                }
+function validarCuentas(a, b){
+  console.log("entre valdiar");
+  var correos = [
+                  {nombre: "Miriam", correo: "miriammp1997@gmail.com", password: "miriammendoza"},
+                  {nombre: "Carmen", correo: "maricarmen@gmail.com", password: "carmenmendoza"},
+                  {nombre: "Lizeth", correo: "lizethlucia@gmail.com", password: "lizethmendoza"}
+                ];
+
+  var  cont = 0;
+  correos.forEach(function(e){
+    if (a == e.correo && b == e.password) {
+      cont++;
+    }
+  });
+
+
+  if(cont == 1){
+    console.log("bien");
+  }
+  else{
+    console.log("persona equivocada")
+  }
+
 
 }
